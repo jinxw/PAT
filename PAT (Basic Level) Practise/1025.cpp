5e66dd5e1537d246ddb1a53c59ae3b41ce40ae5d
jinxw <jinxw07@outlook.com>
2017-09-10 21:03:54 +0800

    1025

--- PAT (Basic Level) Practise/1025.cpp
+++ PAT (Basic Level) Practise/1025.cpp
@@ -2,8 +2,10 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+using namespace std;
 class node{
 public:
+	node()=default;
 	friend ostream &operator<<(ostream &,const node &);
 	friend istream &operator>>(ostream &,node &);
 private:
@@ -11,38 +13,43 @@ private:
 	int data;
 	int next;
 };
-ostream &operator<<(ostream &os,const node &n){
+std::ostream &operator<<(ostream &os,const node &n){
 	os<<n.address<<" "<<n.data<<" "<<n.next;
 	return os;
 }
-istream &operator>>(istream &is,node &n){
+std::istream &operator>>(istream &is,node &n){
 	is>>n.address>>n.data>>n.next;	//没有处理可能失败的情况
 	return is;
 }
-std::list<node> & ReadNode(std::istream &in,std::size_t n){
-	std::list<node> ln();
-	for(std::size_t i = 0;i<n;i++){
-		node tmp;
-		in>>tmp.address>>tmp.data>>tmp.next;
-		ln.push_back(tmp);
-	}
-	return ln;
-}
-void sortLn(std::list<node> ln&, int first_address){
-	auto first_iter = find_if(ln.begin(),ln.end(),[first_address](const node n&){n.address == first_address});
-	ln.push_front(*first_iter);
-	ln.erase(first_iter);
-}
-void printLn(std::list<node> ln&,int k){
-	std::vector<node> vn(ln.begin(),ln.end());
-	for_each(vn.begin(),vn.end(),[](const node n&){std::cout<<n.address<<" "<<n.data<<" "<<n.next<<std::endl})
-}
+// std::list<node> &ReadNode(std::istream &in,std::size_t n){
+// 	std::list<node> ln();
+// 	for(std::size_t i = 0;i<n;i++){
+// 		node tmp;
+// 		in>>tmp;
+// 		ln.push_back(tmp);
+// 	}
+// 	return ln;
+// }
+// void sortLn(std::list<node> ln&, int first_address){
+// 	auto first_iter = find_if(ln.begin(),ln.end(),[first_address](const node n&){n.address == first_address});
+// 	ln.push_front(*first_iter);
+// 	ln.erase(first_iter);
+// }
+// void printLn(std::list<node> &ln,int k){
+// 	for_each(ln.begin(),ln.end(),[](const node &n){std::cout<<n<<std::endl});
+// 	// std::vector<node> vn(ln.begin(),ln.end());
+// 	// for_each(vn.begin(),vn.end(),[](const node n&){std::cout<<n.address<<" "<<n.data<<" "<<n.next<<std::endl})
+// }
 int main()
 {
 	int first_address,n,k;
 	std::cin>>first_address>>n>>k;
-	auto ln& = ReadNode(cin,n);
-	sortLn(ln,first_address);
-	printLn(ln,k);
+	// auto ln = ReadNode(std::cin,n);
+	// sortLn(ln,first_address);
+	// std::list<node> ln = {node(1000,1,1001)};
+	// printLn(ln,k);
+	node tmp();
+	std::cin>>tmp;
+	std::cout<<tmp;
 	return 0;
 }
