cf1df7e2641d90ac3121874139e62a99ae39218f
jinxw <jinxw07@outlook.com>
2018-01-26 16:22:23 +0800

    1047

--- PAT (Basic Level) Practise/1047.cpp
+++ PAT (Basic Level) Practise/1047.cpp
@@ -3,20 +3,18 @@
 #include <map>
 #include <utility>
 int main(){
-    size_t n;
+    int n;
     std::cin>>n;
-    size_t team,member,score;
-    std::map<size_t,size_t> sum_score;
-    for(size_t i=0;i<n;++i){
+    int team,member,score;
+    std::map<int,int> sum_score;
+    for(int i=0;i<n;++i){
         scanf("%d-%d%d",&team,&member,&score);
         sum_score[team]+=score;
     }
-    std::pair<size_t,size_t> max;
+    std::pair<int,int> max;
     for(const auto &n:sum_score){
-        std::cout<<max.first<<" "<<max.second<<std::endl;   //
         if(n.second>=max.second){
-            max.first = n.first;
-            max.second = n.second;
+            max = n;
         }
     }
     std::cout<<max.first<<" "<<max.second<<std::endl;
