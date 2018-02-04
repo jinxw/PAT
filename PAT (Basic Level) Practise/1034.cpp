2e8cc77958bbe62f6f3b3763a960e6cc13225f04
jinxw <jinxw07@outlook.com>
2018-02-04 19:41:42 +0800

    1034

--- PAT (Basic Level) Practise/1034.cpp
+++ PAT (Basic Level) Practise/1034.cpp
@@ -11,15 +11,15 @@ public:
     friend std::istream &operator>>(std::istream &,Fraction &);
     friend std::ostream &operator<<(std::ostream &,const Fraction &);
     void toIrreducible();
+	bool isZero();
 private:
     int minus = 1;  //1正 -1负 
-    int k = 0;
     int n;
     int m;
 };
 const Fraction Fraction::operator+(const Fraction &rhs) const{
     Fraction sum;
-    sum.n = this->minus*(this->k*this->m + this->n)*rhs.m + this->m*(rhs.n + rhs.k*rhs.m)*rhs.minus;
+    sum.n = this->minus*this->n*rhs.m + this->m*rhs.n*rhs.minus;
     sum.m = this->m*rhs.m;
     sum.toIrreducible();
     return sum;
@@ -32,16 +32,13 @@ const Fraction Fraction::operator-(const Fraction &rhs) const{
 const Fraction Fraction::operator*(const Fraction &rhs) const{
     Fraction res;
     res.minus = this->minus * rhs.minus;
-    res.n = (this->k*this->m + this->n)*(rhs.k*rhs.m + rhs.n);
+    res.n = this->n*rhs.n;
     res.m = this->m * rhs.m;
     res.toIrreducible();
     return res;
 }
 const Fraction Fraction::operator/(const Fraction &rhs) const{
-    Fraction tmp(rhs);
-    tmp.n += tmp.k*tmp.m;
-    std::swap(tmp.n,tmp.m);
-    tmp.k = 0;
+    Fraction tmp(rhs.m,rhs.n);
     tmp.toIrreducible();
     return this->operator*(tmp);
 }
@@ -56,23 +53,27 @@ std::istream &operator>>(std::istream &is,Fraction &f){
     return is;
 }
 std::ostream &operator<<(std::ostream &os,const Fraction &f){
+	if(f.n == 0){
+		os<<"0";
+		return os;
+	}
     if(f.minus == -1)
-        std::cout<<"(-";
-    if(f.k!=0){
-        std::cout<<f.k;
-        if(f.n!=0){
-            std::cout<<" ";
+        os<<"(-";
+    if(f.n/f.m != 0){
+        os<<f.n/f.m;
+        if(f.n%f.m!=0){
+            os<<" ";
         }
     }
-    if(f.n!=0)
-        os<<f.n<<"/"<<f.m;
+    if(f.n%f.m!=0)
+        os<<f.n%f.m<<"/"<<f.m;
     if(f.minus == -1)
         std::cout<<")";
     return os;
 }
 void Fraction::toIrreducible(){
-    this->k += this->n / this->m;
-    this->n %= this->m;
+    //this->k += this->n / this->m;
+    //this->n %= this->m;
     for(int i=this->n;i>=2;--i){    //可优化
         if(this->n%i==0 && this->m%i==0){
             this->n /= i;
@@ -80,6 +81,12 @@ void Fraction::toIrreducible(){
             break;
         }
     }
+}
+bool Fraction::isZero(){
+	if(this->n == 0)
+		return true;
+	else
+		return false;
 }
 
 int main(){
@@ -90,6 +97,9 @@ int main(){
     std::cout<<a<<" + "<<b<<" = "<<a+b<<std::endl;
     std::cout<<a<<" - "<<b<<" = "<<a-b<<std::endl;
     std::cout<<a<<" * "<<b<<" = "<<a*b<<std::endl;
-    std::cout<<a<<" / "<<b<<" = "<<a/b<<std::endl;
+	if(b.isZero())
+    	std::cout<<a<<" / "<<b<<" = "<<a/b<<std::endl;
+	else
+		std::cout<<a<<" / "<<b<<" = "<<a/b<<std::endl;
     return 0;
 }
