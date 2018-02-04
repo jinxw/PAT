dc02c7561860ad056ea82d15599f5bdb1d75e718
jinxw <jinxw07@outlook.com>
2018-02-04 17:42:43 +0800

    1051

--- PAT (Basic Level) Practise/1051.c
+++ PAT (Basic Level) Practise/1051.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14159265
-
 int main(){
 	double r1,p1,r2,p2;
 	scanf("%lf%lf%lf%lf",&r1,&p1,&r2,&p2);
@@ -12,6 +10,14 @@ int main(){
 	double b2 = r2 * sin(p2);
 	double a = a1*a2 - b1*b2;
 	double b = a1*b2 + a2*b1;
-	printf("%.2f%c%.2fi",a,b<0 ? '\0' : '+',b);
+	//保留2位小数时会造成-0.00
+	if(a >= -0.005 && a <= 0)
+		a = 0;
+	if(b >= -0.005 && b <= 0)
+		b = 0;
+	if(b < 0)
+		printf("%.2f%.2fi\n",a,b);
+	else
+		printf("%.2f+%.2fi\n",a,b);
 	return 0;
 }
