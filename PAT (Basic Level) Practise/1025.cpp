1d429414eb11c3ce265b515834b1ea435b480b19
jinxw <jinxw07@outlook.com>
2018-02-07 18:22:22 +0800

    1025

--- PAT (Basic Level) Practise/1025.cpp
+++ PAT (Basic Level) Practise/1025.cpp
@@ -1,90 +1,38 @@
 #include <iostream>
-#include <list>
+#include <cstdio>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 #include <algorithm>
-#include <string>
-class node{
-public:
-	node()=default;
-	node(int a,int b,int c):address(a),data(b),next(c){};
-	friend std::ostream &operator<<(std::ostream &,const node &);
-	friend std::istream &operator>>(std::istream &,node &);
-	int get_address() const {
-		return this->address;
-	}
-	int get_next() const {
-		return this->next;
-	}
-	void set_next(int n) {
-		next = n;
-	}
-private:
-	int address;
-	int data;
-	int next;
-};
-std::ostream &operator<<(std::ostream &os,const node &n){
-	std::string s_address(std::to_string(n.address));
-	std::string s_next(std::to_string(n.next));
-	os<<std::string(5-s_address.size(),'0')+s_address<<" "<<n.data<<" "<<(n.next==-1?"-1":std::string(5-s_next.size(),'0')+s_next);
-	return os;
-}
-std::istream &operator>>(std::istream &is,node &n){
-	is>>n.address>>n.data>>n.next;	//没有处理可能失败的情况
-	return is;
-}
 
-std::list<node>::iterator ln_iter_add (std::list<node>::iterator iter,std::size_t n){
-	for(int i=0;i<n;i++){
-		++iter;
-	}
-	return iter;
-}
-
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	int first_address,n,k;
-	std::cin>>first_address>>n>>k;
-
-	std::list<node> ln;
-	for(std::size_t i =0;i<n;i++){
-		node tmp(0,0,0);
-		std::cin>>tmp;
-		ln.push_back(tmp);
-	}
-
-	auto iter = ln.end();
-	int next_address = -1 ;
-	while(iter != ln.begin()){
-		auto pre_iter = find_if(ln.begin(),ln.end(),[next_address](const node &n){return n.get_next() == next_address;});
-		ln.insert(iter,*pre_iter);
-		ln.erase(pre_iter);
-		--iter;
-		next_address = iter->get_address();
-	}
-	
-	iter = ln.begin();
-	for(int j=0;j<n/k;j++){
-		for(int i=k-1;i>0;--i){
-			auto tmp = ln_iter_add(iter,i);
-			ln.insert(iter,*tmp);
-			ln.erase(tmp);
-		}
-		++iter;
-	}
-
-	auto first = ln.begin(),second = ++ln.begin();
-	while(second!=ln.end()){
-		first->set_next(second->get_address());
-		++first;
-		++second;
-	}
-	first->set_next(-1);
-
-	for(auto &n:ln){
-		std::cout<<n<<std::endl;
-	}
-	std::cout<<std::flush;
-
-	return 0;
+int main(){
+    int first_addr,n,k;
+    scanf("%d%d%d",&first_addr,&n,&k);
+    //还可以直接把addr当作数组下标，那样速度更快。
+    std::unordered_map<int,std::pair<int,int>> map;
+    for(int i=0;i<n;++i){
+        int addr;
+        scanf("%d",&addr);
+        scanf("%d%d",&map[addr].first,&map[addr].second);
+    }
+    std::vector<std::pair<int,int>> list;    //addr,data
+    for(int next_addr=first_addr;next_addr!=-1;){
+        auto &tmp = map[next_addr];
+        list.push_back(std::make_pair(next_addr,tmp.first));
+        next_addr = tmp.second;
+    }
+    for(int i=0;i+k<=list.size();i+=k){
+        std::reverse(list.begin()+i,list.begin()+i+k);
+    }
+    bool is_first = true;
+    for (const auto &a : list){
+        if (is_first){
+            printf("%05d %d ", a.first, a.second);
+            is_first = false;
+        }else{
+            printf("%05d\n%05d %d ", a.first, a.first, a.second);
+        }
+    }
+    printf("-1\n");
+    return 0;
 }
